8911f0e4493574f0f30ea492a926573acf259650
dunkcoder <dunkcoder@gmail.com>
2014-08-31 16:44:50 +0800

    refresh

--- src/main/java/com/dunkcoder/s3h3/util/EncryptedDataSource.java
+++ src/main/java/com/dunkcoder/s3h3/util/EncryptedDataSource.java
@@ -33,7 +33,7 @@ public class EncryptedDataSource extends DriverManagerDataSource {
     }
 
     public static void main(String[] args) {
-        String password = "root"; // passw0rd
+        String password = "pa55w0rd"; // pa55w0rd
         BASE64Encoder encoder = new BASE64Encoder();
         String encryptedPassword = encoder.encode(password.getBytes());
         System.out.println(encryptedPassword);
